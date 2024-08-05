c980ced5a6b3ccc3ffb797d0a13833f6558067fa
Akashdeep Singh <singhakash0796@gmail.com>
2024-08-05 12:33:32 -0400

    remove debug

--- educationsystem/urls.py
+++ educationsystem/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('education.urls'))
-] + debug_toolbar_urls()
+] 
